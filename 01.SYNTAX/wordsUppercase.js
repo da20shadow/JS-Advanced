33aeb0cbfe8ae695db60db0244580d678950f698
vipmehmed <vipmehmed99@gmail.com>
2022-05-24 19:45:48 +0300

    Task 9 Words uppercase

--- 01.SYNTAX/wordsUppercase.js
+++ 01.SYNTAX/wordsUppercase.js
@@ -1,1 +1,15 @@
 //Task 9 - Words Uppercase
+function uppercaseWords(string){
+    let arr = string.match(/\w+/g);
+    let upperArr =[];
+    for (const word of arr) {
+        upperArr.push(word.toUpperCase());
+    }
+    console.log(upperArr.join(', '))
+}
+uppercaseWords('Hi, how are you? bro')
+//Second Solution
+function uppercaseWordsSecondSolution(string){
+    console.log(string.match(/\w+/g).join(', ').toLocaleUpperCase());
+}
+uppercaseWordsSecondSolution('Hi, how are you?');
