08ae9f961c36321412987e1339167258e0da9386
vipmehmed <vipmehmed99@gmail.com>
2022-05-24 09:16:22 +0300

    Task 2 - Greatest Common Divisor

--- 01.SYNTAX/exerciseSyntax.js
+++ 01.SYNTAX/exerciseSyntax.js
@@ -9,3 +9,44 @@ function calculateMoneyForFruits(fruitType, grams, pricePerKg) {
   console.log(`I need $${money} to buy ${weight} kilograms ${fruitType}.`);
 }
 
+//Task 2 - Greatest Common Divisor
+//Solution 1
+function getGreatestCommonDivisor(firstNumber,secondNumber){
+    
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
+
+    if (firstNumber > secondNumber){
+        console.log(getGreatestDivisor(firstNumber))
+    } else {
+        console.log(getGreatestDivisor(secondNumber))
+    }
+    
+    function getGreatestDivisor(number){
+        let greatestNumber;
+        for(let i = 1; i <= number; i++){
+            
+            if (firstNumber % i === 0 && secondNumber % i === 0) {
+                greatestNumber = i;
+            }
+        }
+        return greatestNumber;
+    };
+
+}
+
+//Solution 2
+function getGreatestCommonDivisorSecondSolution(firstNum, secondNum) {
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
+
+    while (firstNum !== secondNum) {
+        if (firstNum > secondNum) {
+            firstNum -= secondNum;
+        } else {
+            secondNum -= firstNum;
+        }
+    }
+    console.log(firstNum);
+}
+
