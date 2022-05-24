5a38fa8ffc1ed9923fd8a7fbd17138f2080a3868
vipmehmed <vipmehmed99@gmail.com>
2022-05-24 19:22:52 +0300

    Task 8 Validity Checker

---
+++ 01.SYNTAX/validityChecker.js
@@ -0,0 +1,16 @@
+// Task 08 - Validity Checker
+function validityChecker(x,y,x1,y1){
+    const isValidDist = (x, y, x1, y1) =>
+        Math.sqrt(Math.pow(x1 - x, 2) + Math.pow(y1 - y, 2)) % 1 === 0
+    const pairs = [
+        [x, y, 0, 0],
+        [x1, y1, 0, 0],
+        [x, y, x1, y1],];
+
+    const printMsg = (a, b, c, d, validity) => `{${a}, ${b}} to {${c}, ${d}} is ${validity}`
+
+    pairs.forEach((x, i) =>
+        console.log(isValidDist(...pairs[i]) ? printMsg(...x, "valid") : printMsg(...x, "invalid"))
+    )
+}
+validityChecker(3,0,0,4)
