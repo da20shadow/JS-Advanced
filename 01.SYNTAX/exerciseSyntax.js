cc7e7732d38729bbd7d1849d03771513595020e3
vipmehmed <vipmehmed99@gmail.com>
2022-05-24 10:23:10 +0300

    Task 3 - Same Numbers

--- 01.SYNTAX/exerciseSyntax.js
+++ 01.SYNTAX/exerciseSyntax.js
@@ -12,7 +12,7 @@ function calculateMoneyForFruits(fruitType, grams, pricePerKg) {
 //Task 2 - Greatest Common Divisor
 //Solution 1
 function getGreatestCommonDivisor(firstNumber,secondNumber){
-    
+
     firstNum = Number(firstNum);
     secondNum = Number(secondNum);
 
@@ -50,3 +50,49 @@ function getGreatestCommonDivisorSecondSolution(firstNum, secondNum) {
     console.log(firstNum);
 }
 
+//Task 3 - Same Numbers
+//Solution 1
+function sameNumbers(number){
+    number = number.toString().split('');
+    let newNumArr = [];
+    number.map(n => newNumArr.push(parseInt(n)));
+    let current;
+    let previous;
+    let isSame = false;
+    let sum = newNumArr.reduce((a,b)=> a + b);
+    for (let i = 0; i < newNumArr.length; i++) {
+        current = newNumArr[i];
+        previous === undefined ? previous = newNumArr[i] : previous = newNumArr[i - 1];
+        if (current === previous){
+            isSame = true;
+        }else {
+            isSame = false;
+            break;
+        }
+    }
+    console.log(isSame);
+    console.log(sum);
+}
+        
+//Solution 2
+function sameNumbersSecondSolution(number){
+    number = number.toString().split('');
+    let newNumArr = [];
+    number.map(n => newNumArr.push(parseInt(n)));
+    let sum = newNumArr.reduce((a,b)=> a + b);
+
+    let areSame = false;
+    let firstNumber = newNumArr[0];
+
+    for (let i = 0; i < newNumArr.length; i++){
+        if (firstNumber !== newNumArr[i]){
+            areSame = false;
+            break;
+        }else {
+            areSame = true;
+        }
+    }
+
+    console.log(areSame);
+    console.log(sum);
+}
