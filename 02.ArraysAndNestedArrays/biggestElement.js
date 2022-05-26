f8f670e6a72f08f08aba0e7253fd2d10ae9ddbd6
vipmehmed <vipmehmed99@gmail.com>
2022-05-26 19:14:08 +0300

    Task 9 biggest element

---
+++ 02.ArraysAndNestedArrays/biggestElement.js
@@ -0,0 +1,20 @@
+function biggestElement(matrix){
+    let length = matrix.length;
+    let element = Number.MIN_VALUE;
+    for (let i = 0; i < length; i++) {
+        let colLength = matrix[i].length;
+        for (let j = 0; j < colLength; j++) {
+           if (element < matrix[i][j]){
+               element = matrix[i][j];
+           }
+        }
+    }
+    return element;
+}
+biggestElement([[20, 50, 10], [8, 33, 145]])
+biggestElement([[3, 5, 7, 12], [-1, 4, 33, 2], [8, 3, 0, 4]])
+
+//Кратко решение:
+function solve(arr) {
+    return Math.max(...arr.flat());
+}
