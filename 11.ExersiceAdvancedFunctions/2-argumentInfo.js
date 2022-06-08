bd40f37164f3e9146f236c1bf1f6b8422a3d8109
vipmehmed <vipmehmed99@gmail.com>
2022-06-08 08:05:04 +0300

    update the solution for the task

--- 11.ExersiceAdvancedFunctions/2-argumentInfo.js
+++ 11.ExersiceAdvancedFunctions/2-argumentInfo.js
@@ -1,6 +1,7 @@
-function argumentInfo(...args){
+function argumentInfo(){
 
-    const countTypes = {};
+    let args = Array.from(arguments);
+    let countTypes = {};
 
     for (const arg of args) {
         const argType = typeof arg;
@@ -8,10 +9,14 @@ function argumentInfo(...args){
         countTypes[argType] ? countTypes[argType] += 1 : countTypes[argType] = 1;
     }
 
-    Object.keys((countTypes))
-        .sort((a,b) => {
-            return countTypes[b] - countTypes[a]
-        })
-        .forEach((key) => console.log(`${key} = ${countTypes[key]}`));
+    // Object.keys((countTypes))
+    //     .sort((a,b) => {
+    //         return countTypes[b] - countTypes[a]
+    //     })
+    //     .forEach((key) => console.log(`${key} = ${countTypes[key]}`));
+    console.log('----------------')
+    countTypes = Object.entries(countTypes).sort((a,b) => b[1] - a[1]);
+    countTypes.forEach(el => console.log(`${el[0]} = ${el[1]}`));
+
 }
 argumentInfo('cat', 42,'kitten',1,3,4, function () { console.log('Hello world!'); })
