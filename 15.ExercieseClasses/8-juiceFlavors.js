7bc963ea8ed8303df2fc3c3a7f696e92fa48bc62
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-06-13 20:06:39 +0300

    Task 8 juice flavors

---
+++ 15.ExercieseClasses/8-juiceFlavors.js
@@ -0,0 +1,53 @@
+function juiceFlavors(fruits){
+
+    let fruitsMap = new Map();
+    let bottles = new Map();
+
+    fruits.forEach(f => {
+        let [fruit,quantity] = f.split(' => ');
+        quantity = Number(quantity);
+
+        if (!fruitsMap.has(fruit)){
+            fruitsMap.set(fruit,quantity);
+        }else {
+            let currentQuantity = fruitsMap.get(fruit);
+            fruitsMap.set(fruit,currentQuantity + quantity);
+        }
+
+        if (fruitsMap.get(fruit) >= 1000){
+            let currentQuantity = fruitsMap.get(fruit);
+
+            while(currentQuantity >= 1000){
+
+                currentQuantity -= 1000;
+
+                if (!bottles.has(fruit)){
+                    bottles.set(fruit,1)
+                }else{
+                    let currentBottles = bottles.get(fruit);
+                    bottles.set(fruit,currentBottles + 1);
+                }
+            }
+            fruitsMap.set(fruit,currentQuantity);
+        }
+
+    })
+
+
+    for (const bottle of bottles) {
+        console.log(bottle.join(' => '))
+    }
+
+}
+juiceFlavors(['Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549'] )
+
+juiceFlavors(['Kiwi => 234',
+    'Pear => 2345',
+    'Watermelon => 3456',
+    'Kiwi => 4567',
+    'Pear => 5678',
+    'Watermelon => 6789'])
